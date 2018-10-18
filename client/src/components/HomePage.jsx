90f39446c0e7b1822c6193bbe7ee3b3cf9bd42b9
Steven <yoeung@gmail.com>
2018-10-18 18:38:58 -0400

    login system

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const HomePage = () => (
+ 
+  <div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Home Page</h1>
+    <p className="lead">Welcome to our Page ! </p>
+  </div>
+</div>
+ 
+);
+
+export default HomePage;
