e00a6336b75425f43b39fa66aaa81a6347d4ec71
Steven <yoeung@gmail.com>
2018-10-18 20:11:51 -0400

    make it better

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -39,7 +39,7 @@ const Base = () => (
   </nav> */}
 
   <nav className="navbar navbar-expand-lg navbar-light bg-light col-md-12 header">
-    My Super Hero List !
+    My Super Hero List
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
   </button>
